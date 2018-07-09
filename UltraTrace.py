a681a63a326e4ed05bb3b1b91c47d50650d177e7
Nathaniel Ziv Stern <nzivstern@gmail.com>
2018-07-09 12:00:37 -0400

    tooltip now works, thanks to code from Alberto Vassena

--- UltraTrace.py
+++ UltraTrace.py
@@ -604,24 +604,24 @@ class TextGridModule(object):
 											 text='frames: ', width=self.label_width, activefill='blue')
 					frames_canvas.bind("<Button-1>", self.getClickedFrame)
 					#put items on canvases
-					# self.fillCanvases()
-					self.app.Trace.frame.update() #this is the main thing
-					self.frame.config(width=self.app.Trace.frame.winfo_width())
-					self.frame.grid_propagate(0)
-					self.frame.update()
-					print("Trace Frame width", self.app.Trace.frame.winfo_width())
-					print(self.frame.winfo_width())
+					self.fillCanvases()
+					# self.app.Trace.frame.update() #this is the main thing
+					# self.frame.config(width=self.app.Trace.frame.winfo_width())
+					# self.frame.grid_propagate(0)
+					# self.frame.update()
+					# print("Trace Frame width", self.app.Trace.frame.winfo_width())
+					# print(self.frame.winfo_width())
 				except:
 					pass
 			# grid the widgets whether we loaded successfully or not
 			# self.CurrentWidgets = self.TkWidgets
 			self.grid()
-			# self.app.Trace.frame.update() #this is the main thing
-			# self.frame.config(width=self.app.Trace.frame.winfo_width())
-			# self.frame.grid_propagate(0)
-			# self.frame.update()
-			# print("Trace Frame width", self.app.Trace.frame.winfo_width())
-			# print(self.frame.winfo_width())
+			self.app.Trace.frame.update() #this is the main thing
+			self.frame.config(width=self.app.Trace.frame.winfo_width())
+			self.frame.grid_propagate(0)
+			self.frame.update()
+			print("Trace Frame width", self.app.Trace.frame.winfo_width())
+			print(self.frame.winfo_width())
 
 	def getFrameTierName(self):
 		'''
@@ -799,6 +799,7 @@ class TextGridModule(object):
 					if tier[i].time >= self.start:
 						x_coord = (tier[i].time-self.start)/duration*self.canvas_width
 						frame = frames.create_line(x_coord, 0, x_coord, self.canvas_height, tags=tier[i].mark)
+						CanvasTooltip(frames, frame,text=tier[i].mark)
 					i+=1
 
 	def my_find_closest(self, event):
@@ -2017,6 +2018,139 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
 	# Print New Line on Complete
 	if iteration == total:
 		print()
+
+class CanvasTooltip:
+    '''
+    It creates a tooltip for a given canvas tag or id as the mouse is
+    above it.
+
+    This class has been derived from the original Tooltip class I updated
+    and posted back to StackOverflow at the following link:
+
+    https://stackoverflow.com/questions/3221956/
+           what-is-the-simplest-way-to-make-tooltips-in-tkinter/
+           41079350#41079350
+
+    Alberto Vassena on 2016.12.10.
+    '''
+
+    def __init__(self, canvas, tag_or_id,
+                 *,
+                 bg='#FFFFEA',
+                 pad=(5, 3, 5, 3),
+                 text='canvas info',
+                 waittime=400,
+                 wraplength=250):
+        self.waittime = waittime  # in miliseconds, originally 500
+        self.wraplength = wraplength  # in pixels, originally 180
+        self.canvas = canvas
+        self.text = text
+        self.canvas.tag_bind(tag_or_id, "<Enter>", self.onEnter)
+        self.canvas.tag_bind(tag_or_id, "<Leave>", self.onLeave)
+        self.canvas.tag_bind(tag_or_id, "<ButtonPress>", self.onLeave)
+        self.bg = bg
+        self.pad = pad
+        self.id = None
+        self.tw = None
+
+    def onEnter(self, event=None):
+        self.schedule()
+
+    def onLeave(self, event=None):
+        self.unschedule()
+        self.hide()
+
+    def schedule(self):
+        self.unschedule()
+        self.id = self.canvas.after(self.waittime, self.show)
+
+    def unschedule(self):
+        id_ = self.id
+        self.id = None
+        if id_:
+            self.canvas.after_cancel(id_)
+
+    def show(self, event=None):
+        def tip_pos_calculator(canvas, label,
+                               *,
+                               tip_delta=(10, 5), pad=(5, 3, 5, 3)):
+
+            c = canvas
+
+            s_width, s_height = c.winfo_screenwidth(), c.winfo_screenheight()
+
+            width, height = (pad[0] + label.winfo_reqwidth() + pad[2],
+                             pad[1] + label.winfo_reqheight() + pad[3])
+
+            mouse_x, mouse_y = c.winfo_pointerxy()
+
+            x1, y1 = mouse_x + tip_delta[0], mouse_y + tip_delta[1]
+            x2, y2 = x1 + width, y1 + height
+
+            x_delta = x2 - s_width
+            if x_delta < 0:
+                x_delta = 0
+            y_delta = y2 - s_height
+            if y_delta < 0:
+                y_delta = 0
+
+            offscreen = (x_delta, y_delta) != (0, 0)
+
+            if offscreen:
+
+                if x_delta:
+                    x1 = mouse_x - tip_delta[0] - width
+
+                if y_delta:
+                    y1 = mouse_y - tip_delta[1] - height
+
+            offscreen_again = y1 < 0  # out on the top
+
+            if offscreen_again:
+                # No further checks will be done.
+
+                # TIP:
+                # A further mod might automagically augment the
+                # wraplength when the tooltip is too high to be
+                # kept inside the screen.
+                y1 = 0
+
+            return x1, y1
+
+        bg = self.bg
+        pad = self.pad
+        canvas = self.canvas
+
+        # creates a toplevel window
+        self.tw = Toplevel(canvas.master)
+
+        # Leaves only the label and removes the app window
+        self.tw.wm_overrideredirect(True)
+
+        win = Frame(self.tw,
+                       background=bg,
+                       borderwidth=0)
+        label = Label(win,
+                          text=self.text,
+                          justify=LEFT,
+                          background=bg,
+                          relief=SOLID,
+                          borderwidth=0,
+                          wraplength=self.wraplength)
+
+        label.grid(padx=(pad[0], pad[2]),
+                   pady=(pad[1], pad[3]),
+                   sticky=NSEW)
+        win.grid()
+
+        x, y = tip_pos_calculator(canvas, label)
+
+        self.tw.wm_geometry("+%d+%d" % (x, y))
+
+    def hide(self):
+        if self.tw:
+            self.tw.destroy()
+        self.tw = None
 
 if __name__=='__main__':
 	app = App()
