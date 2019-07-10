c809832babb2a11a96ed0b3239edd0e8ddee70e3
Nathaniel Ziv Stern <nzivstern@gmail.com>
2019-07-10 12:39:25 -0400

    begin work on drag rectangle

--- UltraTrace.py
+++ UltraTrace.py
@@ -114,6 +114,8 @@ class ZoomFrame(Frame):
 		self.canvas = Canvas( master,  bg='grey', width=self.canvas_width, height=self.canvas_height, highlightthickness=0 )
 		self.canvas.grid(row=0, column=0, sticky='news')
 		self.canvas.update() # do i need
+		rect = RectTracker(self.canvas)
+		rect.autodraw()
 
 		# self.master.rowconfigure(0, weight=1) # do i need
 		# self.master.columnconfigure(0, weight=1) # do i need
@@ -243,6 +245,42 @@ class Header(Label):
 		Wrapper for Tk Label() object with a specified font
 		'''
 		Label.__init__(self, master, text=text, font='TkDefaultFont 12 bold')
+
+class RectTracker:
+	''' Copied from http://code.activestate.com/recipes/577409-python-tkinter-canvas-rectangle-selection-box/'''
+
+	def __init__(self, canvas):
+		self.canvas = canvas
+		self.item = None
+
+	def draw(self, start, end, **opts):
+		"""Draw the rectangle"""
+		return self.canvas.create_rectangle(*(list(start)+list(end)), **opts)
+
+	def autodraw(self, **opts):
+		"""Setup automatic drawing; supports command option"""
+		self.start = None
+		self.canvas.bind("<Button-1>", self.__update, '+')
+		self.canvas.bind("<B1-Motion>", self.__update, '+')
+		self.canvas.bind("<ButtonRelease-1>", self.__stop, '+')
+
+		self._command = opts.pop('command', lambda *args: None)
+		self.rectopts = opts
+
+	def __update(self, event):
+		if not self.start:
+			self.start = [event.x, event.y]
+			return
+
+		if self.item is not None:
+			self.canvas.delete(self.item)
+		self.item = self.draw(self.start, (event.x, event.y), **self.rectopts)
+		self._command(self.start, (event.x, event.y))
+
+	def __stop(self, event):
+		self.start = None
+		self.canvas.delete(self.item)
+		self.item = None
 
 class Crosshairs(object):
 	def __init__(self, zframe, x, y, color, transform=True):
