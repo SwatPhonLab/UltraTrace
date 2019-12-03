3718e52d0467e398c0991c81cd98a6fbd0f90264
Kevin Murphy <keggsmurph21@gmail.com>
2019-12-02 18:23:57 -0800

    Separate out PlaybackModule into its own file

--- ultratrace/modules/__init__.py
+++ ultratrace/modules/__init__.py
@@ -1,5 +1,6 @@
 from .dicom import DicomModule
 from .metadata import MetadataModule
+from .playback import PlaybackModule
 from .spectrogram import SpectrogramModule
 from .textgrid import TextGridModule
 from .trace import TraceModule
