ebea6116358a6a46309850b1e5bb21e0feba5e1e
Kevin Murphy <keggsmurph21@gmail.com>
2019-12-02 18:16:17 -0800

    Separate out SpectrogramModule into its own file

--- ultratrace/modules/__init__.py
+++ ultratrace/modules/__init__.py
@@ -1,4 +1,5 @@
 from .dicom import DicomModule
 from .metadata import MetadataModule
+from .spectrogram import SpectrogramModule
 from .textgrid import TextGridModule
 from .trace import TraceModule
