131b0c856bfcacac8755b15ca98b407235071870
Kevin Murphy <keggsmurph21@gmail.com>
2019-12-02 18:40:10 -0800

    Remove unused code from __main__.py

--- ultratrace/__main__.py
+++ ultratrace/__main__.py
@@ -5,50 +5,17 @@ from . import util
 from .util.logging import *
 from .widgets import *
 
-# core libs
-from tkinter import *
-from tkinter.ttk import *
-from tkinter import filedialog
-# import soundfile as sf
-# import numpy as np
-# import scipy.fftpack as fftpack
-# import urllib.request as request
-import argparse, datetime, json, \
-	math, os, sys, random, shutil, warnings, decimal, re,\
-	soundfile as sf, scipy.fftpack as fftpack, urllib.request as request #FIXME should I put these with non-critical dependencies?
-
-import parselmouth
-import copy
-
-import base64
-
-# monkeypatch the warnings module
-warnings.showwarning = lambda msg, *args : warn(msg)
-
-# critical dependencies
-from magic import Magic # sudo -H pip3 install -U python-magic
-getMIMEType = Magic(mime=True).from_file
+import argparse
+import os
 import PIL
 
-# non-critical dependencies
+from tkinter import *
+
 try:
-	import cairosvg # sudo -H pip3 install cairosvg && brew install cairo
-	import wav2vec  # sudo -H pip3 install wav2vec
-	_WAV_VIS_LIBS_INSTALLED = True
-except (ImportError):
-	warnings.warn('Waveform visualization library failed to load')
-	_WAV_VIS_LIBS_INSTALLED = False
-try:
-    debug('Loading platform-specific enhancements for ' + util.get_platform())
-    if util.get_platform() == 'Linux':
-        import xrp  # pip3 install xparser
-        from ttkthemes import ThemedTk
-        from pathlib import Path
-    else:
-        from ttkthemes import ThemedTk
-except (ImportError):
-    warnings.warn('Can\'t load platform-specific enhancements')
+    from ttkthemes import ThemedTk
+except ImportError as e:
     ThemedTk = Tk
+    warn(e)
 
 class App(ThemedTk):
 	'''
@@ -71,6 +38,9 @@ class App(ThemedTk):
 		# do the normal Tk init stuff
 		if util.get_platform()=='Linux':
 			try:
+				info('Loading platform-specific enhancements for Linux')
+				import xrp  # pip3 install xparser
+				from pathlib import Path
 				Xresources = xrp.parse_file(os.path.join(str(Path.home()), '.Xresources'))
 				if '*TtkTheme' in Xresources.resources:
 					ttktheme = Xresources.resources['*TtkTheme']
@@ -83,7 +53,7 @@ class App(ThemedTk):
 					info("Falling back to default Linux Tk theme: {}".format(ttktheme))
 				super().__init__(theme=ttktheme)
 			except Exception as e:
-				error("exception: ", e)
+				error(e)
 				super().__init__()
 		else:
 			super().__init__()
@@ -628,7 +598,7 @@ class App(ThemedTk):
 		except ValueError:
 			error( 'Please enter an integer!' )
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 25, fill = '█'):
+def printProgressBar(iteration, total, prefix = '', suffix = '', decimals = 1, length = 25, fill = '█'):
 	"""
 	Call in a loop to create terminal progress bar
 	@params:
@@ -647,139 +617,6 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
 	# Print New Line on Complete
 	if iteration == total:
 		print()
-
-class CanvasTooltip:
-    '''
-    It creates a tooltip for a given canvas tag or id as the mouse is
-    above it.
-
-    This class has been derived from the original Tooltip class I updated
-    and posted back to StackOverflow at the following link:
-
-    https://stackoverflow.com/questions/3221956/
-           what-is-the-simplest-way-to-make-tooltips-in-tkinter/
-           41079350#41079350
-
-    Alberto Vassena on 2016.12.10.
-    '''
-
-    def __init__(self, canvas, tag_or_id,
-                 *,
-                 bg='#FFFFEA',
-                 pad=(5, 3, 5, 3),
-                 text='canvas info',
-                 waittime=400,
-                 wraplength=250):
-        self.waittime = waittime  # in miliseconds, originally 500
-        self.wraplength = wraplength  # in pixels, originally 180
-        self.canvas = canvas
-        self.text = text
-        self.canvas.tag_bind(tag_or_id, "<Enter>", self.onEnter)
-        self.canvas.tag_bind(tag_or_id, "<Leave>", self.onLeave)
-        self.canvas.tag_bind(tag_or_id, "<ButtonPress>", self.onLeave)
-        self.bg = bg
-        self.pad = pad
-        self.id = None
-        self.tw = None
-
-    def onEnter(self, event=None):
-        self.schedule()
-
-    def onLeave(self, event=None):
-        self.unschedule()
-        self.hide()
-
-    def schedule(self):
-        self.unschedule()
-        self.id = self.canvas.after(self.waittime, self.show)
-
-    def unschedule(self):
-        id_ = self.id
-        self.id = None
-        if id_:
-            self.canvas.after_cancel(id_)
-
-    def show(self, event=None):
-        def tip_pos_calculator(canvas, label,
-                               *,
-                               tip_delta=(10, 5), pad=(5, 3, 5, 3)):
-
-            c = canvas
-
-            s_width, s_height = c.winfo_screenwidth(), c.winfo_screenheight()
-
-            width, height = (pad[0] + label.winfo_reqwidth() + pad[2],
-                             pad[1] + label.winfo_reqheight() + pad[3])
-
-            mouse_x, mouse_y = c.winfo_pointerxy()
-
-            x1, y1 = mouse_x + tip_delta[0], mouse_y + tip_delta[1]
-            x2, y2 = x1 + width, y1 + height
-
-            x_delta = x2 - s_width
-            if x_delta < 0:
-                x_delta = 0
-            y_delta = y2 - s_height
-            if y_delta < 0:
-                y_delta = 0
-
-            offscreen = (x_delta, y_delta) != (0, 0)
-
-            if offscreen:
-
-                if x_delta:
-                    x1 = mouse_x - tip_delta[0] - width
-
-                if y_delta:
-                    y1 = mouse_y - tip_delta[1] - height
-
-            offscreen_again = y1 < 0  # out on the top
-
-            if offscreen_again:
-                # No further checks will be done.
-
-                # TIP:
-                # A further mod might automagically augment the
-                # wraplength when the tooltip is too high to be
-                # kept inside the screen.
-                y1 = 0
-
-            return x1, y1
-
-        bg = self.bg
-        pad = self.pad
-        canvas = self.canvas
-
-        # creates a toplevel window
-        self.tw = Toplevel(canvas.master)
-
-        # Leaves only the label and removes the app window
-        self.tw.wm_overrideredirect(True)
-
-        win = Frame(self.tw,
-                       # background=bg,
-                       borderwidth=0)
-        label = Label(win,
-                          text=self.text,
-                          justify=LEFT,
-                          background=bg,
-                          relief=SOLID,
-                          borderwidth=0,
-                          wraplength=self.wraplength)
-
-        label.grid(padx=(pad[0], pad[2]),
-                   pady=(pad[1], pad[3]),
-                   sticky=NSEW)
-        win.grid()
-
-        x, y = tip_pos_calculator(canvas, label)
-
-        self.tw.wm_geometry("+%d+%d" % (x, y))
-
-    def hide(self):
-        if self.tw:
-            self.tw.destroy()
-        self.tw = None
 
 if __name__=='__main__':
 	app = App()
