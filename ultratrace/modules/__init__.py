a6fb721909ebde54f84ee0a693174f7773087ae3
Kevin Murphy <keggsmurph21@gmail.com>
2019-12-02 18:28:19 -0800

    Separate out SearchModule into its own file

--- ultratrace/modules/__init__.py
+++ ultratrace/modules/__init__.py
@@ -2,6 +2,7 @@ from .control import ControlModule
 from .dicom import DicomModule
 from .metadata import MetadataModule
 from .playback import PlaybackModule
+from .search import SearchModule
 from .spectrogram import SpectrogramModule
 from .textgrid import TextGridModule
 from .trace import TraceModule
