5d5c41044e7c54e4005d3ffa51f53ee6eddb9c90
Kevin Murphy <keggsmurph21@gmail.com>
2019-12-06 09:05:33 -0800

    Fix missing **kwargs in logging.info signature (#94)
    This patch allows us to pass arbitrary arguments along to print().
    As mentioned in #94, trying to call info('...', end='\r') raises an
    error since we don't know about the end= parameter.  This bug crept
    in when I changed all the calls to print() to use the logging funcs.

--- ultratrace/util/logging.py
+++ ultratrace/util/logging.py
@@ -1,21 +1,21 @@
 import sys
 
-def log(*msgs):
-    print(*map(str, msgs), file=sys.stderr)
+def log(*msgs, **kwargs):
+    print(*map(str, msgs), **kwargs, file=sys.stderr)
 
-def debug(*msgs):
-    log('DEBUG:', *msgs)
+def debug(*msgs, **kwargs)
+    log('DEBUG: ', *msgs, **kwargs)
 
 def info(*msgs):
-    log('INFO:', *msgs)
+    log('INFO:  ', *msgs, **kwargs)
 
 def warn(*msgs):
-    log('WARN:', *msgs)
+    log('WARN:  ', *msgs, **kwargs)
 
 def error(*msgs):
-    log('ERROR:', *msgs)
+    log('ERROR: ', *msgs, **kwargs)
 
 def severe(*msgs):
-    log('SEVERE:', *msgs)
+    log('SEVERE:', *msgs, **kwargs)
 
 __all__ = [ 'debug', 'info', 'warn', 'error', 'severe' ]
