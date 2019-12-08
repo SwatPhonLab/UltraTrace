e95e2a28c0627ff17d72c2dc22a630db1644e99d
Kevin Murphy <keggsmurph21@gmail.com>
2019-12-08 13:23:00 -0800

    Fix syntax errors (missing ":" and "**kwargs")

--- ultratrace/util/logging.py
+++ ultratrace/util/logging.py
@@ -3,19 +3,19 @@ import sys
 def log(*msgs, **kwargs):
     print(*map(str, msgs), **kwargs, file=sys.stderr)
 
-def debug(*msgs, **kwargs)
+def debug(*msgs, **kwargs):
     log('DEBUG: ', *msgs, **kwargs)
 
-def info(*msgs):
+def info(*msgs, **kwargs):
     log('INFO:  ', *msgs, **kwargs)
 
-def warn(*msgs):
+def warn(*msgs, **kwargs):
     log('WARN:  ', *msgs, **kwargs)
 
-def error(*msgs):
+def error(*msgs, **kwargs):
     log('ERROR: ', *msgs, **kwargs)
 
-def severe(*msgs):
+def severe(*msgs, **kwargs):
     log('SEVERE:', *msgs, **kwargs)
 
 __all__ = [ 'debug', 'info', 'warn', 'error', 'severe' ]
