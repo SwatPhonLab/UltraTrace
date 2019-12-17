9da2c0e9adcfe8c804995416ef68dba433e6cc7b
Caleb Ho <caleb.yh.ho@gmail.com>
2019-12-17 15:19:43 -0800

    Add install nox session

--- noxfile.py
+++ noxfile.py
@@ -5,3 +5,9 @@ import nox
 def format(session):
     session.install("black")
     session.run("black", "--check", "ultratrace2")
+
+
+@nox.session
+def install(session):
+    """Install ultratrace2. Assumes required system libraries are installed."""
+    session.install(".")
