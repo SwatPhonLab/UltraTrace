58e1f623cdb54c542f7ff7e928f506b9763bdd94
Kevin Murphy <keggsmurph21@gmail.com>
2019-12-17 17:10:25 -0800

    Trace: implement some of the Trace/TraceList/XHair API

--- ultratrace2/model/trace.py
+++ ultratrace2/model/trace.py
@@ -1,28 +1,79 @@
-from typing import Optional
+from typing import ClassVar, Dict, Set, TYPE_CHECKING
+from uuid import uuid4, UUID
 
-from uuid import uuid4
-from .color import Color, get_random_color
+from .color import Color, get_random_color, RED
+from .xhair import XHair
+
+if TYPE_CHECKING:
+    from .files.bundle import FileBundle
 
 
 class Trace:
-    def __init__(self, name: str, color: Optional[Color] = None):
-        self.id = uuid4()
-        self.is_visible = True
+
+    DEFAULT_TRACE_NAME: ClassVar[str] = "tongue"
+    DEFAULT_TRACE_COLOR: ClassVar[Color] = RED
+
+    def __init__(self, name: str, color: Color):
+        self.id: UUID = uuid4()
+        self.is_visible: bool = True
+        self.xhairs: Dict[FileBundle, Dict[int, Set[XHair]]] = {}
         self.name = name
         self.color = color
 
-    def change_color(self, new_color: Optional[Color] = None) -> Optional[Color]:
+    def get_color(self) -> Color:
+        return self.color
+
+    def change_color(self, new_color: Color) -> Color:
         if new_color is None:
             new_color = get_random_color()
         old_color = self.color
         self.color = new_color
         return old_color
 
+    def get_name(self) -> str:
+        return self.name
+
     def change_name(self, new_name: str) -> str:
         old_name = self.name
         self.name = new_name
         return old_name
 
+    def show(self) -> None:
+        self.is_visible = True
+
+    def hide(self) -> None:
+        self.is_visible = False
+
+    def add_xhair(self, bundle: FileBundle, frame: int, xhair: XHair) -> None:
+        if bundle not in self.xhairs:
+            self.xhairs[bundle] = {}
+        if frame not in self.xhairs[bundle]:
+            self.xhairs[bundle][frame] = set()
+        self.xhairs[bundle][frame].add(xhair)
+
 
 class TraceList:
-    pass
+    def __init__(self):
+        self.traces: List[Trace] = []
+        default_trace = self.add_trace(
+            Trace.DEFAULT_TRACE_NAME, Trace.DEFAULT_TRACE_COLOR,
+        )
+        self.default_trace: Trace = default_trace
+        self.selected_trace: Trace = default_trace
+
+    def add_trace(self, name: str, color: Color) -> Trace:
+        trace = Trace(name, color)
+        self.traces.append(trace)
+        return trace
+
+    def get_default_trace(self) -> Trace:
+        return self.default_trace
+
+    def set_default_trace(self, trace: Trace) -> None:
+        self.default_trace = trace
+
+    def get_selected_trace(self) -> Trace:
+        return self.selected_trace
+
+    def set_selected_trace(self, trace: Trace) -> None:
+        self.selected_trace = trace
---
+++ ultratrace2/model/xhair.py
@@ -0,0 +1,53 @@
+from typing import Tuple, TYPE_CHECKING, Union
+from uuid import uuid4
+
+if TYPE_CHECKING:
+    from .color import Color
+    from .trace import Trace
+
+
+class XHair:
+    def __init__(self, trace: Trace, x: float, y: float):
+
+        self.id = uuid4()
+        self.trace = trace
+        self.x = x
+        self.y = y
+
+        self.is_selected = False
+        self.is_hidden = False
+
+    def sq_dist_from(self, other: Union[XHair, Tuple[float, float]]) -> float:
+        # Euclidean distance squared, since sqrt() is relatively slow :^)
+        if isinstance(other, XHair):
+            dx = self.x - other.x
+            dy = self.y - other.y
+        elif isinstance(other, tuple):
+            dx = self.x - other[0]
+            dy = self.y - other[1]
+        return (dx ** 2) + (dy ** 2)
+
+    def __repr__(self):
+        return f"XHair(id={self.id}, x={self.x}, y={self.y})"
+
+    def toggle_select(self) -> None:
+        self.is_selected = not self.is_selected
+
+    def select(self) -> None:
+        self.is_selected = True
+
+    def unselect(self) -> None:
+        self.is_selected = False
+
+    def show(self):
+        self.is_hidden = False
+
+    def hide(self):
+        self.is_hidden = True
+
+    def move(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+
+    def get_color(self) -> Color:
+        return self.trace.get_color()
