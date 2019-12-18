ab2836bdf7ca315440796cb3c72e494f658a878d
Caleb Ho <caleb.yh.ho@gmail.com>
2019-12-17 17:23:21 -0800

    Fix CI runs

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 if which brew &> /dev/null; then
     brew install portaudio libmagic ffmpeg libav
+elif which apt-get &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install \
+        portaudio19-dev libportaudio2 \
+        libmagic-dev \
+        ffmpeg \
+        libav-tools
 else
-    echo "Homebrew is not on your PATH. Is it installed?"
-    echo "Other package managers are not currently supported"
+    echo "Your package manager is not currently supported."
+    echo "Currently supported managers are: brew, apt."
     exit 1
 fi
