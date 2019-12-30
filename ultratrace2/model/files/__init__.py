dfa0c6d93c411a82955399235c1b16446ec62e2f
Kevin Murphy <keggsmurph21@gmail.com>
2019-12-29 21:04:14 -0800

    FileLoaders: Add try/except around importing loaders
    This way, if we don't have the required libraries for a particular
    loader, we'll just throw out a warning when we try to register the
    loader instead of erroring out.  This means that we'll just never
    be able to load files that require libraries that we don't have.
    
    Closes #121

--- ultratrace2/model/files/__init__.py
+++ ultratrace2/model/files/__init__.py
@@ -1,32 +1,70 @@
+import logging
+
 from .registry import register_loader_for_extensions_and_mime_types as __register
 
-from .loaders import DICOMLoader
-from .loaders import FLACLoader
-from .loaders import MeasurementLoader
-from .loaders import MP3Loader
-from .loaders import OggLoader
-from .loaders import TextGridLoader
-from .loaders import WAVLoader
+
+logger = logging.getLogger(__name__)
 
 
-__register(
-    [".dicom", ".dcm"], ["application/dicom"], DICOMLoader,
-)
-__register(
-    [], [], FLACLoader,
-)
-__register(
-    [], [], MeasurementLoader,
-)
-__register(
-    [], [], MP3Loader,
-)
-__register(
-    [], [], OggLoader,
-)
-__register(
-    [".textgrid"], ["text/plain"], TextGridLoader,
-)
-__register(
-    [".wav"], ["audio/x-wav", "audio/wav"], WAVLoader,
-)
+try:
+    from .loaders import DICOMLoader
+
+    __register(
+        [".dicom", ".dcm"], ["application/dicom"], DICOMLoader,
+    )
+except ImportError as e:
+    logger.warning(e)
+
+try:
+    from .loaders import FLACLoader
+
+    __register(
+        [], [], FLACLoader,
+    )
+except ImportError as e:
+    logger.warning(e)
+
+try:
+    from .loaders import MeasurementLoader
+
+    __register(
+        [], [], MeasurementLoader,
+    )
+except ImportError as e:
+    logger.warning(e)
+
+try:
+    from .loaders import MP3Loader
+
+    __register(
+        [], [], MP3Loader,
+    )
+except ImportError as e:
+    logger.warning(e)
+
+try:
+    from .loaders import OggLoader
+
+    __register(
+        [], [], OggLoader,
+    )
+except ImportError as e:
+    logger.warning(e)
+
+try:
+    from .loaders import TextGridLoader
+
+    __register(
+        [".textgrid"], ["text/plain"], TextGridLoader,
+    )
+except ImportError as e:
+    logger.warning(e)
+
+try:
+    from .loaders import WAVLoader
+
+    __register(
+        [".wav"], ["audio/x-wav", "audio/wav"], WAVLoader,
+    )
+except ImportError as e:
+    logger.warning(e)
