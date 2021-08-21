712d9726201fbca3b7613242904e0b0dd43ba635
Jonathan Washington <jonathan.n.washington@gmail.com>
2021-08-21 15:38:34 -0400

    more work on controls

--- ultratrace/__main__.py
+++ ultratrace/__main__.py
@@ -141,7 +141,7 @@ class Frame(wx.Frame):
 		#self.pady=3
 		#self.columnconfigure(0,weight=1)
 		#self.rowconfigure(0,weight=1)
-		text1 = wx.StaticText(panel, label="controls here")
+		#text1 = wx.StaticText(panel, label="controls here")
 		text2 = wx.StaticText(panel, label="US here")
 		text3 = wx.StaticText(panel, label="labels here")
 		text4 = wx.StaticText(panel, label="audio and textgrids here")
@@ -149,30 +149,72 @@ class Frame(wx.Frame):
 		self.controlBox = wx.BoxSizer(wx.VERTICAL)
 		####  Buttons
 		####  FileSelectorBox
+		##### FileControlBox
+		self.fileControlBox = wx.BoxSizer(wx.HORIZONTAL)
+		self.fileSelectorOpen = wx.Button(self, id=wx.ID_OPEN)#, style=wx.BU_NOTEXT)
+		self.fileControlBox.Add(self.fileSelectorOpen)
+
 		self.fileSelectorBox = wx.StaticBoxSizer(wx.VERTICAL, self, label="Files")
+		self.fileSelectorBox.Add(self.fileControlBox)
 		self.fileSelector = wx.ListCtrl(self, -1, style=wx.LC_REPORT)
 		self.fileSelector.InsertColumn(0, 'name', width=50)
 		self.fileSelector.InsertColumn(1, 'annotated', wx.LIST_FORMAT_RIGHT, width=50)
 		self.fileSelector.InsertItem(0, 'test')
 
 		self.fileSelectorBox.Add(self.fileSelector, 1, wx.EXPAND)
+
 		####  FrameSelectorBox
 		self.frameSelectorBox = wx.StaticBoxSizer(wx.HORIZONTAL, self, label="Frames")
-		self.frameSelectorPrev = wx.Button(self, label="<")
+		self.frameSelectorPrev = wx.Button(self, label="<", style=wx.BU_EXACTFIT)
 		self.frameSelectorText = wx.TextCtrl(self, style=wx.TE_RIGHT, value=self.frameSV)
-		size = self.frameSelectorText.GetSizeFromTextSize(self.frameSelectorText.GetTextExtent('99999'))
-		self.frameSelectorText.SetInitialSize(size)
-		#self.framesEntryText = Entry(self.framesSubframe, width=5, textvariable=self.frameSV)
-		self.frameSelectorNext = wx.Button(self, label=">")
-		self.frameSelectorBox.AddMany([(self.frameSelectorPrev),(self.frameSelectorText),(self.frameSelectorNext)])
+		self.frameSelectorNext = wx.Button(self, label=">", style=wx.BU_EXACTFIT)
+		self.buttonPlayPause = wx.Button(self, label="⏯")# style=wx.BU_EXACTFIT)
+		self.setCharSize(self.frameSelectorNext, 1)
+		self.setCharSize(self.frameSelectorText, 5)
+		self.setCharSize(self.frameSelectorPrev, 1)
+		self.setCharSize(self.buttonPlayPause, 3)
+
+		#self.frameSelectorBox.AddMany([(self.frameSelectorPrev),(self.frameSelectorText),(self.frameSelectorNext),(self.buttonPlayPause)])
+		self.frameSelectorBox.Add(self.frameSelectorPrev, flag=wx.LEFT, border=10)
+		self.frameSelectorBox.Add(self.frameSelectorText)
+		self.frameSelectorBox.Add(self.frameSelectorNext, flag=wx.RIGHT, border=10)
+		self.frameSelectorBox.Add(self.buttonPlayPause, border=10)
 
 		####  AnnotationsBox
+		self.annotationsBox = wx.StaticBoxSizer(wx.HORIZONTAL, self, label="Annotations")
+		self.buttonSelectAll = self.newIconButton(wx.ID_SELECTALL, "gtk-select-all")
+		self.buttonCopy = self.newIconButton(wx.ID_COPY, wx.ART_COPY)
+		self.buttonPaste = self.newIconButton(wx.ID_PASTE, wx.ART_PASTE) #wx.Button(self, id=wx.ID_PASTE, style=wx.BU_NOTEXT | wx.BU_EXACTFIT)
+		self.buttonDelete = self.newIconButton(wx.ID_DELETE, wx.ART_DELETE) #wx.Button(self, id=wx.ID_DELETE, style=wx.BU_NOTEXT | wx.BU_EXACTFIT)
+		self.buttonZoomIn = self.newIconButton(wx.ID_ZOOM_IN, "gtk-zoom-in") #wx.Button(self, id=wx.ID_ZOOM_IN, style=wx.BU_NOTEXT | wx.BU_EXACTFIT)
+		self.buttonZoomOut = self.newIconButton(wx.ID_ZOOM_OUT, "gtk-zoom-out") #wx.Button(self, id=wx.ID_ZOOM_OUT, style=wx.BU_NOTEXT | wx.BU_EXACTFIT)
+		self.buttonZoomFit = self.newIconButton(wx.ID_ZOOM_FIT, "gtk-zoom-fit") #wx.Button(self, id=wx.ID_ZOOM_FIT, style=wx.BU_NOTEXT | wx.BU_EXACTFIT)
+		self.buttonUndo = self.newIconButton(wx.ID_UNDO, wx.ART_UNDO)
+		self.buttonRedo = self.newIconButton(wx.ID_REDO, wx.ART_REDO)
+
+		#self.annotationsBox.AddMany([(self.buttonSelectAll),(self.buttonCopy),(self.buttonPaste),(self.buttonDelete)])
+		self.annotationsBox.Add(self.buttonSelectAll, flag=wx.LEFT, border=10)
+		self.annotationsBox.Add(self.buttonCopy, flag=wx.BOTTOM, border=10)
+		self.annotationsBox.Add(self.buttonPaste)
+		self.annotationsBox.Add(self.buttonDelete)
+		self.annotationsBox.Add((20,0))
+		self.annotationsBox.Add(self.buttonUndo)
+		self.annotationsBox.Add(self.buttonRedo, flag=wx.RIGHT, border=10)
+
+
+		####  ViewBox
+		self.viewBox = wx.StaticBoxSizer(wx.HORIZONTAL, self, label="View")
+		self.viewBox.Add(self.buttonZoomIn, flag=wx.LEFT, border=10)
+		self.viewBox.Add(self.buttonZoomOut, flag=wx.BOTTOM, border=10)
+		self.viewBox.Add(self.buttonZoomFit, flag=wx.RIGHT, border=10)
 
 		####  LayersBox
+		self.layersBox = wx.StaticBoxSizer(wx.VERTICAL, self, label="Labels")
+		#self.layersBox = 
 		
 
 		## add all sections to the control box
-		self.controlBox.AddMany([(text1, 1, wx.EXPAND), (self.fileSelectorBox, 1, wx.EXPAND), (self.frameSelectorBox,1,wx.EXPAND)])
+		self.controlBox.AddMany([(self.fileSelectorBox, 1, wx.EXPAND), ((0,10)), (self.frameSelectorBox,0,wx.EXPAND), ((0,10)), (self.annotationsBox, 0, wx.EXPAND), ((0,10)), (self.viewBox,0,wx.EXPAND), ((0,10)), (self.layersBox,0,wx.EXPAND)])
 
 		self.grid.AddMany([(self.controlBox, 1, wx.EXPAND), (text2, 1, wx.EXPAND), (text3), (text4)])
 
@@ -226,13 +268,22 @@ class Frame(wx.Frame):
 		# force window to front
 		#self.lift()
 
+	def setCharSize(self, obj, sizeInChar):
+		size = obj.GetSizeFromTextSize(obj.GetTextExtent('9'*sizeInChar))
+		obj.SetInitialSize(size)
+		obj.SetSize(size)
+
+	def newIconButton(self, wxId, wxBmp):
+		button = wx.Button(self, id=wxId, style=wx.BU_NOTEXT | wx.BU_EXACTFIT)
+		button.SetBitmapLabel(wx.ArtProvider.GetBitmap(wxBmp,wx.ART_MENU))
+		return button
+
 	def lift(self):
 		'''
 		Bring window to front (doesn't shift focus to window)
 		'''
 		self.attributes('-topmost', 1)
 		self.attributes('-topmost', 0)
-
 
 	def update(self):
 		pass
