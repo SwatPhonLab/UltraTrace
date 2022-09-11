27fc44f049074a9d53c7b0b0804ee8c31a9e4613
Ark Lu <39932933+qiyoulu@users.noreply.github.com>
2022-09-11 12:59:15 -0400

    dnf support

--- install.sh
+++ install.sh
@@ -4,12 +4,15 @@ if which brew &> /dev/null; then
     brew install portaudio ffmpeg libav
 elif which apt-get &> /dev/null; then
     sudo apt-get update
-    sudo apt-get install \
+    sudo apt-get install python3 \
         portaudio19-dev libportaudio2 \
         ffmpeg \
         libav-tools
+elif which dnf &> /dev/null; then
+    sudo dnf up
+    sudo dnf in python3 portaudio ffmpeg
 else
     echo "Your package manager is not currently supported."
-    echo "Currently supported managers are: brew, apt."
+    echo "Currently supported managers are: brew, apt, dnf."
     exit 1
 fi
