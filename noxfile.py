7cd8559f497af62f9f675e66105af839e191769b
Kevin Murphy <keggsmurph21@gmail.com>
2022-10-22 19:07:49 -0400

    nox: Just try installing `ultratrace`

--- noxfile.py
+++ noxfile.py
@@ -2,31 +2,6 @@ import nox
 
 
 @nox.session
-def format(session):
-    session.install("black")
-    session.run("black", "--check", "ultratrace2")
-
-
-@nox.session
 def install(session):
-    """Install ultratrace2. Assumes required system libraries are installed."""
+    """Install ultratrace. Assumes required system libraries are installed."""
     session.install(".")
-
-
-@nox.session
-def typecheck(session):
-    """Run static analyzer against source files."""
-    session.install(".[dev]")
-    session.run("mypy", "ultratrace2")
-
-
-@nox.session
-def lint(session):
-    session.install(".[dev]")
-    session.run("flake8", "ultratrace2")
-
-
-@nox.session
-def tests(session):
-    session.install(".[dev]")
-    session.run("pytest", "ultratrace2")
