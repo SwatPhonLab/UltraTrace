96538e6803d5415761ead626fbee27ff1edafd14
Ark Lu <39932933+qiyoulu@users.noreply.github.com>
2022-12-20 10:47:56 -0500

    update install.sh for dnf to work with wx

--- install.sh
+++ install.sh
@@ -4,12 +4,15 @@ if which brew &> /dev/null; then
     brew install portaudio ffmpeg libav
 elif which apt-get &> /dev/null; then
     sudo apt-get update
-    sudo apt-get install \
+    sudo apt-get install python3 \
         portaudio19-dev libportaudio2 \
-        ffmpeg \
-        libav-tools
+        ffmpeg libav-tools
+elif which dnf &> /dex/null; then
+	sudo dnf up
+	sudo dnf in python3-devel python3-pillow-tk \
+		portaudio-devel ffmpeg python3-wxpython4
 else
     echo "Your package manager is not currently supported."
-    echo "Currently supported managers are: brew, apt."
+    echo "Currently supported managers are: brew, apt, dnf."
     exit 1
 fi
