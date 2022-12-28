f4291a8cf819bcd5ed35e1dc46bd342b7dd030b2
qiyoulu <qiyou.lu1022@gmail.com>
2022-12-27 19:14:16 -0500

    remove libav-tools dependency

--- install.sh
+++ install.sh
@@ -4,13 +4,12 @@ if which brew &> /dev/null; then
     brew install portaudio ffmpeg libav
 elif which apt-get &> /dev/null; then
     sudo apt-get update
-    sudo apt-get install python3 \
-        portaudio19-dev libportaudio2 \
-        ffmpeg libav-tools
+    sudo apt-get install python3 ffmpeg \
+        portaudio19-dev libportaudio2 wxpython-tools
 elif which dnf &> /dex/null; then
 	sudo dnf up
-	sudo dnf in python3-devel python3-pillow-tk \
-		portaudio-devel ffmpeg python3-wxpython4
+	sudo dnf in python3-devel ffmpeg-devel \
+		portaudio-devel python3-wxpython4
 else
     echo "Your package manager is not currently supported."
     echo "Currently supported managers are: brew, apt, dnf."
